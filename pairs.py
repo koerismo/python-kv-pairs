c5c15be201648eea7df717b3d8318f9dafcc368c
DrMeepster <19316085+DrMeepster@users.noreply.github.com>
2021-03-24 22:07:11 -0700

    types (and a few other things lol)

--- pairs.py
+++ pairs.py
@@ -1,8 +1,12 @@
 # Pairs class v2
 
-class Pair:
+from typing import Generic, List, TypeVar, Union
+
+V = TypeVar('V')
+
+class Pair(Generic[V]):
     ''' A class to handle key/value pairs. '''
-    def __init__(self, key, values):
+    def __init__(self, key: str, values: Union[V, List[V]]) -> None:
         self.key = key
         if isinstance(values, list):
             self.values = values
@@ -11,24 +15,34 @@ class Pair:
 
     # Retrieve property from Pair
 
-    def __getitem__(self, key):
+    def __getitem__(self, key: Union[str, int]):
         if isinstance(key, int):
             return self.values[key]
         if not isinstance(key, str):
             raise TypeError('__getitem__ in Pair: Key must be int or str!')
         
-        return [x for x in self.values if x.key == key]
+        return [x for x in self.values if isinstance(x, Pair) and x.key == key]
     
     # Delete items from the Pair
 
-    def __delitem__(self,key):
-        del self.values[key]
+    def __delitem__(self, key: Union[str, int]) -> None:
+        if isinstance(key, int):
+            del self.values[key]
+        if not isinstance(key, str):
+            raise TypeError('__getitem__ in Pair: Key must be int or str!')
+
+        for idx, x in enumerate(self.values):
+            if isinstance(x, Pair) and x.key == key:
+                del self.values[idx]
 
     # Represent Pair in string form
     
-    def __repr__(self):
+    def __repr__(self) -> str:
         if len(self.values) == 0:
             return f'Pair(key={self.key})'
         if len(self.values) == 1:
             return f'Pair(key={self.key},value={self.values[0]})'
         return f'Pair(key={self.key},values={self.values})'
+
+    key: str
+    values: List[V]
